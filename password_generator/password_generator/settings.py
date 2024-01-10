f339928ef232d1b0feaecd2f3a8423f649ced88b
иван кедров <ivkedrov@gmail.com>
2024-01-10 16:12:01 +0300

    commit

--- password_generator/password_generator/settings.py
+++ password_generator/password_generator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'foo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = ['vanci.pythonanywhere.com'
 
 ]
 
